37a36e5a567f39eab1107da260ea1954fea1d8b5
ethanschafstall <ethans9999@gmail.com>
2024-01-08 23:42:30 +0100

    Updated Code, Added new functionality, comments, Renamed const
    Random snake spawn: Added functions for spawning a snake with random starting position and move direction.
    
    Game reset: Changed how game is reset. A new snake, Apple are spawned and score is reset VS reloading the page like before.
    
    Renamed all const values to capital letters
    
    Removed some variables which weren't being used, or that were redundent

--- Code/snake-js-game-main/src/main.js
+++ Code/snake-js-game-main/src/main.js
@@ -3,35 +3,27 @@ import { Snake } from './Snake.js';
 import { Segment } from './Segment.js';
 import { Apple } from './Apple.js';
 
-// general settings
+// general settings.
 const CANVAS = document.querySelector('canvas');
 const CTX = CANVAS.getContext('2d');
 const PIXEL_SIZE = 40;
-const REFRESH_RATE = 1000;
+const REFRESH_RATE = 100;
 
-// snake starting positions
-const SNAKE_START_X = PIXEL_SIZE;
-const SNAKE_START_Y = PIXEL_SIZE*4;
-
-// Game border widths/heights
+// Game border widths/heights, the limits of where the snake and apple are allowed to spawn.
 const BOARDER_START = 0;
 const BOARDER_LIMIT = 800;
 
-// Apple spawn limits
-const APPLE_SPAWN_START = (BOARDER_START/PIXEL_SIZE) + PIXEL_SIZE/PIXEL_SIZE;
-const APPLE_SPAWN_LIMIT = (BOARDER_LIMIT/PIXEL_SIZE) - 2 * PIXEL_SIZE/PIXEL_SIZE;
-
-// Gameboard widths/heights
+// Gameboard widths/heights, where snake and apples spawn.
 const GAMEBOARD_START = BOARDER_START + PIXEL_SIZE;
 const GAMEBOARD_LIMIT = BOARDER_LIMIT - PIXEL_SIZE * 2;
 
-
+// General variables.
 let snake;
 let apple;
 let score = 0;
 let gameStart = true;
-let hasPressedSpace;
-// listener for
+
+// listener for key presses.
 document.addEventListener('keydown', (event) => {
   var code = event.code;
   updateMoveDirection(code);
@@ -39,11 +31,11 @@ document.addEventListener('keydown', (event) => {
 
 const move = () => {
 
-  // Draw gameboard limits
+  // Draw gameboard limits.
   CTX.fillStyle = 'DarkRed';
   CTX.fillRect(BOARDER_START, BOARDER_START, BOARDER_LIMIT, BOARDER_LIMIT);
 
-  // Draw gameboard screen
+  // Draw gameboard screen.
   CTX.fillStyle = 'black';
   CTX.fillRect(GAMEBOARD_START, GAMEBOARD_START, GAMEBOARD_LIMIT, GAMEBOARD_LIMIT);
 
@@ -53,23 +45,23 @@ const move = () => {
   CTX.textAlign = "center";
   CTX.fillText(`Score: ${score}`, GAMEBOARD_LIMIT / 2 + 16, GAMEBOARD_START - PIXEL_SIZE / 5);
 
-  // Timer
+  // Timer.
   setTimeout(() => {
     requestAnimationFrame(move);
   }, REFRESH_RATE);
 
   
   // Checks if the just started, if true then spawns both the snake and apple.
-  gameStart ? (snake = new Snake(SNAKE_START_X , SNAKE_START_Y, 'r'), apple = spawnApple(), gameStart = false): undefined;
+  gameStart ? (snake = spawnSnake(), apple = spawnApple(), gameStart = false): undefined;
 
-  if (hasPressedSpace){
-  // Draws the snake
+  
+  // Draws the snake.
   snake.segments.forEach((e, index, array) => {
 
     // Check if the index (the snake segment to be drawn) is the last of the array meaning the head of the snake.
-    const isHead = index === array.length - 1;
+    const IS_HEAD = index === array.length - 1;
 
-    drawSegment(e.x, e.y, isHead);
+    drawSegment(e.x, e.y, IS_HEAD);
   });
 
   // Create new snake head
@@ -80,23 +72,56 @@ const move = () => {
   // Conditions to check for colisions. If apple colision then spawn new apple, grow snake.
   // If snake colision then alert user of gamestate, reload game.
   checkAppleColision() ? (apple = spawnApple(), createSegment(), score++) : undefined;
-  checkSnakeColision() ? (alert("You lost!"), location.reload()) : undefined;
-  hasPressedSpace = false;
-}
+  checkSnakeColision() ? (alert("You lost!"), gameStart=true, score = 0) : undefined;
 };
 
 requestAnimationFrame(move);
 
-// Function for drawing a snake segment
+// Spawns a snake in a random part of the gameboard and a "random" direction (based on position, not entirely random).
+const spawnSnake = () => {
+  let randomX;
+  let randomY;
+
+  // Generates random x and y position values until both are a multiple of 40 (the pixel size).
+  do {
+    randomX = getRandomPosition(GAMEBOARD_START, GAMEBOARD_LIMIT);
+    randomY = getRandomPosition(GAMEBOARD_START, GAMEBOARD_LIMIT);
+  } while (!(randomX % PIXEL_SIZE === 0 && randomY % PIXEL_SIZE === 0));
+
+  // Gets the direction the snake should based on it's spawning position.
+  const DIRECTION = determineDirection(randomX, randomY);
+
+  return new Snake(randomX, randomY, DIRECTION);
+}
+
+// Function to get a random position within a specified range.
+const getRandomPosition = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// Function to determine the traval direction of the snake depending on the largest distance from a border.
+const determineDirection = (x, y) => {
+  // 4 const with values equating to the x/y distance to a border.
+  const [DISTANCE_LEFT, DISTANCE_RIGHT, DISTANCE_TOP, DISTANCE_BOTTOM] = [x - GAMEBOARD_START, GAMEBOARD_LIMIT - x, y - GAMEBOARD_START, GAMEBOARD_LIMIT - y];
+  
+  // max value of those 4 const
+  const MAX_DISTANCE = Math.max(DISTANCE_LEFT, DISTANCE_RIGHT, DISTANCE_TOP, DISTANCE_BOTTOM);
+
+
+  return MAX_DISTANCE === DISTANCE_LEFT ? "l" : MAX_DISTANCE === DISTANCE_RIGHT ? "r" : MAX_DISTANCE === DISTANCE_TOP ? "u" : "d";
+};
+
+// Function for drawing a snake segment.
 const drawSegment = (x, y, altColor) => {
   altColor ?  CTX.fillStyle = 'orange' : CTX.fillStyle = 'red'
   CTX.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
 }
 
-// Function for creating a new snake segment
+// Function for creating a new snake segment.
 const createSegment = () => {
 
-  // Creates a new segment with the same position values as the first segement in the array (the snake's tail)
+  // Creates a new segment with the same position values as the first segement in the array (the snake's tail).
   const { x, y } = snake.segments[0];
   snake.segments.unshift(new Segment(x, y));
 }
@@ -155,36 +180,37 @@ const updateMoveDirection = (keyPressed) => {
         snake.direction = 'r';
       }
       break;  
-    case "Space":
-      hasPressedSpace = true;
-      break;
   }
 }
 
 // Function that returns an apple object which x & y positions that aren't equal to any of the snake segments x & y pos.
 const spawnApple = () => {
   
-  let appleXPos;
-  let appleYPos;
-  
-  let randomXNum;
-  let randomYNum;
+  const MAX_COORDINATE = GAMEBOARD_LIMIT / PIXEL_SIZE;
+  const MIN_COORDINATE = GAMEBOARD_START / PIXEL_SIZE;
 
-  let xNums = snake.segments.map(a => a.x);
-  let yNums = snake.segments.map(a => a.y);
+  let newX;
+  let newY;
+  
+  let randomX;
+  let randomY;
+
+  // Extracts all of the x and y values from each snake segment objects and saves into an array.
+  const X_VALUES = snake.segments.map(a => a.x);
+  const Y_VALUES = snake.segments.map(a => a.y);
   
 
-  // Generates random numbers between 1-18 (gameboard limits), while the numbers don't equate to any of the segment x & y values
+  // Generates random numbers between 1-18 (gameboard limits), while the numbers don't equate to any of the segment x & y values.
   do {
-    randomXNum = Math.floor(Math.random() * APPLE_SPAWN_LIMIT)+ APPLE_SPAWN_START;
-    randomYNum = Math.floor(Math.random() * APPLE_SPAWN_LIMIT)+ APPLE_SPAWN_START;
-    } while (xNums.includes(randomXNum) && yNums.includes(randomYNum));
+    randomX = Math.floor(Math.random() * MAX_COORDINATE) + MIN_COORDINATE;
+    randomY = Math.floor(Math.random() * MAX_COORDINATE) + MIN_COORDINATE;
+    } while (X_VALUES.includes(randomX) && Y_VALUES.includes(randomY));
   
-    // apples x and y positions between the randomly generated values, multiplied by pixelsize so 
-  appleXPos = randomXNum*PIXEL_SIZE;
-  appleYPos = randomYNum*PIXEL_SIZE;
+  // new x and y values become the randomly generated values, multiplied by pixelsize so the position fits within gameboard grid.
+  newX = randomX * PIXEL_SIZE;
+  newY = randomY * PIXEL_SIZE;
 
-  return new Apple(appleXPos, appleYPos);
+  return new Apple(newX, newY);
 
 }
 // Function which draws apple using it's x and y cords.
@@ -197,20 +223,20 @@ const drawApple = () => {
 // Function which which returns a conditional variable if the apple and snake head are overlapping.
 const checkAppleColision = () => {
 
-  const head = snake.segments[snake.segments.length-1];
+  const HEAD = snake.segments[snake.segments.length-1];
 
   // Check if the apple and snake head are overlapping.
-  return apple.x == head.x && apple.y == head.y ? true : false;
+  return apple.x == HEAD.x && apple.y == HEAD.y ? true : false;
 }
 
 // Function which checks for the snakes colisions, returns a true boolean if the snake is colliding something it shouldn't.
 const checkSnakeColision = () => {
 
-  const head = snake.segments[snake.segments.length - 1];
+  const HEAD = snake.segments[snake.segments.length - 1];
   // condition for if the snake is out of gameboard bounderies.
-  const isOutOfBounds = head.y < GAMEBOARD_START || head.y > GAMEBOARD_LIMIT || head.x < GAMEBOARD_START || head.x > GAMEBOARD_LIMIT;
+  const IS_OUT_OF_BOUNDS = HEAD.y < GAMEBOARD_START || HEAD.y > GAMEBOARD_LIMIT || HEAD.x < GAMEBOARD_START || HEAD.x > GAMEBOARD_LIMIT;
   // condition for if snake is overlapping with itself.
-  const isBodyOverlap = snake.segments.slice(0, snake.segments.length - 1).some(element => element.x === head.x && element.y === head.y);
+  const IS_BODY_OVERLAP = snake.segments.slice(0, snake.segments.length - 1).some(element => element.x === HEAD.x && element.y === HEAD.y);
  
-  return isOutOfBounds || isBodyOverlap;
+  return IS_OUT_OF_BOUNDS || IS_BODY_OVERLAP;
 }
